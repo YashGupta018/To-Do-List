02ee63d2b748062e6a7b31ec5437c7c95dc2653d
Yash Gupta <ygupta2001@gmail.com>
2023-01-11 16:31:49 +0530

    Final Commit (Created 2 pvt methods)

--- app/src/main/java/com/yashgupta/todolist/MainActivity.java
+++ app/src/main/java/com/yashgupta/todolist/MainActivity.java
@@ -2,13 +2,76 @@ package com.yashgupta.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        // We are using the adapter here which is used to pass notes through the adapter
+        listView.setAdapter(itemsAdapter);
+        // We need to get the notes from listView so we are using the adapter
+        setUpListViewListener();
+
+    }
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+                items.remove(i);
+                // This will specify the exact position of the item we want to remove
+                itemsAdapter.notifyDataSetChanged();
+                // This will update the adapter with the new positions of the items
+                return true;
+            }
+        });
+    }
+
+    private void addItem(View view) {
+        EditText input = findViewById(R.id.editText);
+        String itemText = input.getText().toString();
+
+        if(!(itemText.equals(""))) {
+            itemsAdapter.add(itemText);
+            input.setText("");
+        }
+        else {
+            Toast.makeText(this, "The field is empty", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
